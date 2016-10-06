75684d5044a80c97cfa53d40cbe9e68bf0fadbac
Jose Maximino Chavez <josemc.ortiz@gmail.com>
2016-10-05 21:08:23 -0400

    Final

---
+++ DAL/script/Parcial1Db.sql
@@ -0,0 +1,11 @@
+﻿create table Materiales(
+		MaterialesId int identity(1,1) primary key,
+		Razon varchar(100)
+)go
+create table MaterialesDetalle(
+		MaterialesDetalleId int identity(1,1)
+		,MaterialesId int references Materiales
+		,Material varchar(30),
+		Cantidad int
+		  
+);
